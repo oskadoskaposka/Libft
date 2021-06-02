b790d98a59c8d6f60a8f5f7b16e934a02b58e450
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-02 12:47:50 -0300

    updating ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,23 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 00:05:02 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/05/26 00:38:04 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/02 12:47:17 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memcpy (void *dest, const char *src, unsigned int len)
 {
-	unsigned int	i;
 	char			*cdest;
 	const char		*csrc;
 
-	i = 0;
-	cdest = dest;
-	csrc = src;
-	while (i < len)
+	cdest = (char *)dest;
+	csrc = (char *)src;
+	while (len > 0)
 	{
-		cdest[i] = csrc[i];
-		i++;
+		*cdest = *csrc;
+		cdest++;
+		csrc++;
+		len--;
 	}
 	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,17 +6,15 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 21:47:32 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/02 12:26:25 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/02 12:46:06 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memmove (void *dest, const void *src, unsigned int len)
 {
-	unsigned int	i;
 	char			*cdest;
 	const char		*csrc;
 
-	i = 0;
 	cdest = (char *)dest;
 	csrc = (char *)src;
 	if (cdest == csrc)
