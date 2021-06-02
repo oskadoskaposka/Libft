b63da9014e06e5d1981a2f99705acb91e79addaa
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-02 11:54:15 -0300

    creation of ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 13:51:38 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/05/30 14:23:54 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/01 19:24:49 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,25 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	unsigned int	size_s2;
 	char			*dest;
 	unsigned int	i;
+	unsigned int	j;
 
+	if (!s1 || !s2)
+		return (NULL);
 	size_s1 = ft_strlen(s1);
 	size_s2 = ft_strlen(s2);
-	dest = malloc (size_s1 + size_s2 + 1);
+	dest = malloc ((size_s1 + size_s2 + 1));
 	i = 0;
-	while (*s2 != '\0')
+	j = 0;
+	while (s1[i] != '\0')
 	{
-		while (*s1 != '\0')
-		{
-			*dest = *s1;
-			s1++;
-		}
-		*dest = *s2;
-		s2++;
+		dest[i] = s1[i];
+		i++;
 	}
-	*dest = '\0';
+	while (s2[j] != '\0')
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
