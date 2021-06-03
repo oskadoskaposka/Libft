2973b06e551e2a44db8f72643f5e83e4bd3cd3e5
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-03 00:58:55 -0300

    correction of strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 16:52:13 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/03 00:56:26 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/03 00:58:46 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(char *big, char *lil, unsigned int len)
 	unsigned int	n;
 
 	i = 0;
-	while (big[i] && i < len)
+	while (big[i] && i <= len)
 	{
 		n = 0;
 		while (lil[n] != '\0' && i + n < len && lil[n] == big [i + n])
