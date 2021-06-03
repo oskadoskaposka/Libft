32ee159d4e7850dd7f71b5a83244c0219b311044
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-03 00:27:47 -0300

    correction of strlcpoy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 01:50:30 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/02 18:45:05 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/03 00:26:23 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 int	ft_strlcpy(char *dest, const char *src, unsigned int size)
 {
+	unsigned int	i;
+
+	i = 0;
 	if (!src)
 		return (0);
 	if (size > 0)
 	{
-		while ((*src != '\0') && (0 < (size - 1)))
+		while ((src[i] != '\0') && (i < (size - 1)))
 		{
-			*dest = *src;
-			dest++;
-			src++;
-			size--;
+			dest[i] = src[i];
+			i++;
 		}
-		*dest = '\0';
+		dest[i] = '\0';
 	}
 	return (ft_strlen(src));
 }
