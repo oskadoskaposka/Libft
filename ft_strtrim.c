d84a2d0dbb1bf83ddffcf9c2edb358b19cacefc2
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-06 19:22:54 -0300

    test strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 14:24:32 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/06 19:21:29 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/06 19:22:53 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	unsigned int	culmination;
 
 	principle = 0;
-	culmination = 0;
-	while ((unsigned char)s1[principle] && ft_strchr(set, (unsigned char)s1[principle]))
+	if (!s1 || !s2)
+		return (NULL);
+	while (s1[principle] && ft_strchr(set, s1[principle]))
 		principle++;
 	culmination = ft_strlen (&s1[principle]);
-	while ((unsigned char)s1[culmination] && ft_strchr(set, (unsigned char)s1[culmination]))
+	while (s1[culmination] && ft_strchr(set, s1[culmination]))
 		culmination--;
 	return (ft_substr(s1, principle, culmination));
 }
