d9403d019591d50deb9a9d84bc85968e0a628610
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-18 01:17:43 -0300

    updating itoa according to the norm

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 20:31:54 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/18 01:17:12 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/18 01:17:39 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 void	ft_putstr_fd(char *str, int fd)
 {
 	if (!str)
-		return (NULL);
+		return;
 	while (*str != '\0')
 	{
 		ft_putchar_fd(*str, fd);
