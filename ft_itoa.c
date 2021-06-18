9e7657322c67a648c9a9fad288b6b47a58d2d876
oskadoskaposka <alepaduanlima@gmail.com>
2021-06-18 00:11:24 -0300

    removing my tests and comments from itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 21:40:58 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/06/18 00:08:34 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/06/18 00:11:20 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,22 @@ char	*ft_itoa(int n)
 	long long int nbr;
 	int digits;
 	char *dest;
-	
-	// unsigned int apagar;
 
 	neg = n < 0;
-	// printf("o valor de neg é: %u \n", neg);
 	nbr = n;
-	// printf("o valor de nbr é: %lld \n", nbr);
 	if (neg)
 		nbr = nbr * -1;
-	// printf("o valor att de nbr é: %lld \n", nbr);
 	digits = 1;
 	while (n / 10 != 0)
 	{
 		n = n / 10;
 		digits++;
 	}
-	// printf("o valor de digits é: %d \n", digits);
 	dest = malloc(neg + digits + 1);
 	if (!dest)
 	{
 		return(NULL);
 	}
-	// apagar = (neg + digits + 1);
-	// printf("a quantidade malocada foi de : %u \n", apagar);
 	dest[neg + digits] = '\0';
 	digits--;
 	if (neg)
@@ -53,25 +45,8 @@ char	*ft_itoa(int n)
 	while (digits > -1)
 	{
 		dest[neg+digits] = nbr % 10 + '0';
-		// printf("digit: %d --- valor: %lli \n", digits, (nbr % 10 + 48));
 		nbr = nbr / 10;
 		digits--;
 	}
-	// printf("o valor de dest é: %s \n", dest);
 	return (dest);
 }
-
-// int main (void)
-// {
-// 	int positivo = 16031993;
-// 	int negativo = INT_MIN;
-
-// 	printf("\npositivo \n----------------------------------------------------------------------------- \n");
-// 	ft_itoa(positivo);
-// 	printf("----------------------------------------------------------------------------- \n\n");
-
-// 	printf("\nnegativo \n----------------------------------------------------------------------------- \n");
-// 	ft_itoa(negativo);
-// 	printf("----------------------------------------------------------------------------- \n\n");
-// 	return (0);
-// }
