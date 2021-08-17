384d6b8fca31915af4f3b62fbd67b1ae8cac0589
oskadoskaposka <alepaduanlima@gmail.com>
2021-08-17 20:37:04 -0300

    Updating atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 14:40:29 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/08/11 22:31:12 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/08/17 20:36:46 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,6 @@
 
 #include "libft.h"
 
-static int	ft_isspace2(char c)
-{
-	if (c == 32 || c == 12 || c == 10 || c == 13 || c == 9 || c == 11)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-static int	ft_transform_sign2(int c)
-{
-	if (c == '-')
-		return (-1);
-	return (1);
-}
-
 int	ft_atoi(const char *str)
 {
 	long int	i;
@@ -40,11 +24,11 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	sign = 1;
-	while (ft_isspace2(*str))
+	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 	{
-		sign *= (ft_transform_sign2(*str));
+		sign *= (ft_transform_sign(*str));
 		str++;
 	}
 	while (ft_isdigit(*str))
